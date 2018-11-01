df643134e2e60bfcfb9dac6950a26568b3c2a630
tim <tim@glo.id.au>
2018-11-01 17:52:17 +1100

    working on async routes + nested routes + passing props down from parent routes

--- source/universal/Routes/Routes.js
+++ source/universal/Routes/Routes.js
@@ -1,33 +1,62 @@
 /* @flow */
 
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import Loadable from 'react-loadable'
 
-import { LoadingPage } from 'universal/pages/LoadingPage/LoadingPage'
+import LoadingPage from 'universal/pages/LoadingPage'
 
-
-const Loading = () => Loadable({
-  loader: () => import('../LoadingPage'),
+const NotFound = Loadable({
+  loader: () => import('universal/pages/NotFoundPage/NotFoundPage'),
   loading: LoadingPage,
 })
 
-const About = () => {
-  return <h2>About ...</h2>;
+const About = ({ routes, account }) => {
+  console.log('ABOUT:', account);
+  return (
+    <div>
+      <h2>Nested About ... </h2>
+      {mapNestedRoutes(routes)}
+    </div>
+  );
 };
 
+class Counter extends React.Component {
+  state = {
+    val: 0,
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ val: this.state.val + 1 }), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <h3>counter value is: {this.state.val}</h3>;
+  }
+}
+
 const Moo = () => {
-  return <h2>Moo cow</h2>;
+  return <h2>Nested Moo cow</h2>;
 };
 
 const App = ({ children, routes }) => {
-  console.log(children);
+  // console.log(children); // undefined
   return (
     <div>
-      <h1>app in here...</h1>
-      {routes.map((route, i) => (
-        <RouteWithSubRoutes key={i} {...route} />
-      ))}
+      <h1>app stuff in here...</h1>
+      <Link to='/'>root</Link><br/>
+      <Link to='/404'>async 404</Link><br/>
+      <Link to='/moo'>/moo</Link><br/>
+      <Link to='/counter'>/counter</Link><br/>
+      <Link to='/about'>/about</Link><br/>
+      <Link to='/about/moo'>/about/moo</Link><br/>
+      {mapNestedRoutes(routes, { account: {
+        wallet: 'moo cow',
+      }})}
     </div>
   );
 }
@@ -35,34 +64,58 @@ const App = ({ children, routes }) => {
 const routes = [
   {
     path: "/",
-    component: App,
+    Component: App,
     routes: [
       {
+        path: "/404",
+        Component: NotFound,
+      },
+      {
         path: "/moo",
-        component: Moo
+        Component: Moo
+      },
+      {
+        path: "/counter",
+        onEnter: () => console.log('counter route/component onEnter!!!'),
+        Component: Counter,
       },
       {
         path: "/about",
-        component: About
-      }
-    ]
-  }
+        Component: About,
+        routes: [
+          {
+            path: "/about/moo",
+            Component: Moo
+          },
+        ],
+      },
+    ],
+  },
 ];
 
-export const Routes = () => {
-  return routes.map((route, i) => {
-    return <RouteWithSubRoutes key={i} {...route} />
-  });
+export const Routes = () => mapNestedRoutes(routes);
+
+function mapNestedRoutes(routes, mergeInProps) {
+  return routes.map((route, i) => (
+    <RouteWithSubRoutes key={i} {...route} mergeInProps={mergeInProps} />
+  ))
 }
 
 function RouteWithSubRoutes(route) {
+  const { Component, path, routes, onEnter, mergeInProps } = route;
   return (
     <Route
       path={route.path}
-      render={props => (
-        // pass the sub-routes down to keep nesting
-          <route.component {...props} routes={route.routes} />
-      )}
+      render={routerProps => {
+        if (typeof onEnter === 'function') onEnter();
+        return (
+          <Component
+            routes={routes}
+            {...routerProps}
+            {...mergeInProps}
+          />
+        );
+      }}
     />
   );
 }
