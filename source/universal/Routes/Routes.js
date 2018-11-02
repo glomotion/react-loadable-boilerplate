81883cd7fbfa65d18c3ad71f3482ec9d25455dc2
tim <tim@glo.id.au>
2018-11-02 14:53:07 +1100

    building out onEnter and onLeave HoC’s

--- source/universal/Routes/Routes.js
+++ source/universal/Routes/Routes.js
@@ -1,16 +1,161 @@
 /* @flow */
 
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import Loadable from 'react-loadable'
 
-import IndexPage from 'universal/pages/IndexPage'
-import NotFoundPage from 'universal/pages/NotFoundPage'
+import LoadingPage from 'universal/pages/LoadingPage'
 
-export const Routes = () => (
-  <Switch>
-    <Route exact path={'/'} component={IndexPage} />
-    <Route component={NotFoundPage} />
-  </Switch>
-)
+
+function withRouteOnEnter(WrappedComponent, onEnter) {
+  return class OnEnterComponent extends Component {
+    componentWillMount() {
+      typeof onEnter === 'function' && onEnter()
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+}
+
+function withRouteOnLeave(WrappedComponent, onLeave) {
+  return class OnEnterComponent extends Component {
+    componentWillUnmount() {
+      typeof onLeave === 'function' && onLeave()
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+}
+
+const NotFound = Loadable({
+  loader: () => import('universal/pages/NotFoundPage/NotFoundPage'),
+  loading: LoadingPage,
+})
+
+const About = ({ routes, account, user }) => {
+  console.log('ABOUT:', account, user);
+  return (
+    <div>
+      <h2>Nested About ... </h2>
+      <div>{JSON.stringify(user)}</div>
+      <div>{JSON.stringify(account)}</div>
+      {mapNestedRoutes(routes)}
+    </div>
+  );
+};
+
+class Counter extends React.Component {
+  state = {
+    val: 0,
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ val: this.state.val + 1 }), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <h3>counter value is: {this.state.val}</h3>;
+  }
+}
+
+const Moo = () => {
+  return <h2>Nested Moo cow</h2>;
+};
+
+const App = ({ children, routes }) => {
+  // console.log(children); // undefined
+  return (
+    <div>
+      <h1>app stuff in here...</h1>
+      <Link to='/'>root</Link><br/>
+      <Link to='/404'>async 404</Link><br/>
+      <Link to='/moo'>/moo</Link><br/>
+      <Link to='/counter'>/counter</Link><br/>
+      <Link to='/about'>/about</Link><br/>
+      <Link to='/about/moo'>/about/moo</Link><br/>
+      {mapNestedRoutes(routes, {
+        account: {
+          wallet: 'moo cow',
+        },
+        user: {
+          id: 'bob the builder',
+          email: 'bob@builder.com',
+        }
+      })}
+    </div>
+  );
+}
+
+const routes = [
+  {
+    path: "/",
+    Component: App,
+    routes: [
+      {
+        path: "/404",
+        Component: NotFound,
+      },
+      {
+        path: "/moo",
+        onLeave: () => console.log('leaving route: /moo'),
+        Component: Moo
+      },
+      {
+        path: "/counter",
+        onEnter: () => console.log('entering route: /counter'),
+        Component: Counter,
+      },
+      {
+        path: "/about",
+        Component: About,
+        routes: [
+          {
+            path: "/about/moo",
+            Component: Moo
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const Routes = () => mapNestedRoutes(routes);
+
+function mapNestedRoutes(routes, mergeInProps) {
+  return routes.map((route, i) => (
+    <RouteWithSubRoutes key={i} {...route} mergeInProps={mergeInProps} />
+  ))
+}
+
+function RouteWithSubRoutes(route) {
+  const { Component, path, routes, onLeave, onEnter, mergeInProps } = route;
+  return (
+    <Route
+      path={route.path}
+      render={routerProps => {
+        const WrappedComponent = withRouteOnLeave(withRouteOnEnter(Component, onEnter), onLeave);
+        return (
+          <WrappedComponent
+            routes={routes}
+            {...routerProps}
+            {...mergeInProps}
+          />
+        );
+      }}
+    />
+  );
+}
 
 export default Routes
+
+
+
+// <Route path="/articles/:id" component={withRouteOnEnter(loadData)(ArticleViewContainer)} />
